ebaa3de6a366d01b74eb310d3804dd31b4227ffa
zdcdada <54224626+zdcdada@users.noreply.github.com>
2022-10-01 01:07:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,15 +39,15 @@ def get_weather(region):
     key = config["weather_key"]
     region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
     response = get(region_url, headers=headers).json()
-    if response["code"] == "404":
-        print("推送消息失败，请检查地区名是否有误！")
-        os.system("pause")
-        sys.exit(1)
-    elif response["code"] == "401":
-        print("推送消息失败，请检查和风天气key是否正确！")
-        os.system("pause")
-        sys.exit(1)
-    else:
+#     if response["code"] == "404":
+#         print("推送消息失败，请检查地区名是否有误！")
+#         os.system("pause")
+#         sys.exit(1)
+#     elif response["code"] == "401":
+#         print("推送消息失败，请检查和风天气key是否正确！")
+#         os.system("pause")
+#         sys.exit(1)
+#     else:
         # 获取地区的location--id
         location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
