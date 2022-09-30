2b05552afd90af3f969ab9a5b12e674435f3f697
zdcdada <54224626+zdcdada@users.noreply.github.com>
2022-10-01 01:12:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_weather(region):
 #         sys.exit(1)
 #     else:
         # 获取地区的location--id
-        location_id = response["location"][0]["id"]
+    location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
     response = get(weather_url, headers=headers).json()
     # 天气
