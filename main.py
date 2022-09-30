e11980e98351b78e89f5aaaab65e482086c01cee
zdcdada <54224626+zdcdada@users.noreply.github.com>
2022-10-01 01:39:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def get_weather(region):
     }
     key = "622262f0b5c3420798ccc3d0d6fc8e06"
     region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    print(region_url)
     response = get(region_url, headers=headers).json()
     print(response)
 #     if response["code"] == "404":
